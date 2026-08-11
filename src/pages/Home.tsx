e30f839b27de4026385aa29f6aaed86375c64e25
sohil-2085 <sohil@techeniac.com>
2026-08-11 18:57:59 +0530

    feat : created login page with the session storage storing auth_token and also add this like if the user is not logged in he or she can not go to the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  return (
-    <div>
-      Home Page
-    </div>
-  )
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if (!sessionStorage.getItem("auth_token")) {
+      navigate("/login");
+    }
+  }, []);
+  return <div>Home Page</div>;
 }
 
-export default Home
+export default Home;
